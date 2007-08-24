b2d0c9d7947f4a2c8efcda862f0d410bd1ef5e97
Sebastien Pierre <sebastien@type-z.org>
2007-08-24 11:12:02 -0400

    Fixed a bug for commands without parameters Commit description tags:        fix

--- Distribution/docommand.py
+++ Distribution/docommand.py
@@ -1,4 +1,4 @@
-# 8< ---[docommand.py]---
+#8< ---[docommand.py]---
 import sys
 __module__ = sys.modules[__name__]
 import re
@@ -26,24 +26,24 @@ class Command(object):
 				argument_number = int(match.group(1))
 				default_argument = match.group(3)
 				default_argument_type = match.group(2)
-				self_1187967376_8310=res
-				self_1187967376_8310.append(["T", expression[offset:match.start()]])
+				self_1187968305_7117=res
+				self_1187968305_7117.append(["T", expression[offset:match.start()]])
 				if default_argument:
-					self_1187967376_8358=res
-					self_1187967376_8358.append(["O", argument_number, default_argument_type, default_argument])
+					self_1187968305_7126=res
+					self_1187968305_7126.append(["O", argument_number, default_argument_type, default_argument])
 				elif True:
-					self_1187967376_8351=res
-					self_1187967376_8351.append(["A", argument_number, None, None])
+					self_1187968305_7164=res
+					self_1187968305_7164.append(["A", argument_number, None, None])
 				offset = match.end()
 			elif True:
-				self_1187967376_8324=res
-				self_1187967376_8324.append(["T", expression[offset]])
+				self_1187968305_7237=res
+				self_1187968305_7237.append(["T", expression[offset:]])
 				offset = len(expression)
 		return res
 	
 	def addArgument(self, defaultValue):
-		self_1187967376_8429=self.arguments
-		self_1187967376_8429.append((defaultValue or ""))
+		self_1187968305_7241=self.arguments
+		self_1187968305_7241.append((defaultValue or ""))
 	
 	"""Returns a list representing the initialized arguments from the given
 	(optional list of argument). When no argument is given, the the default
@@ -52,11 +52,11 @@ class Command(object):
 		if arguments is None: arguments = []
 		res=[]
 		for a in arguments:
-			self_1187967376_8471=res
-			self_1187967376_8471.append(a)
+			self_1187968305_7234=res
+			self_1187968305_7234.append(a)
 		while (len(res) < len(self.arguments)):
-			self_1187967376_8433=res
-			self_1187967376_8433.append(self.arguments[(len(res) - 1)])
+			self_1187968305_7323=res
+			self_1187968305_7323.append(self.arguments[(len(res) - 1)])
 		return res
 	
 	def getArity(self):
@@ -70,28 +70,31 @@ class Command(object):
 		arg_count=0
 		for element in self.parsedExpression:
 			if (element[0] == "T"):
-				self_1187967376_852=res
-				self_1187967376_852.append(element[1])
+				self_1187968305_7343=res
+				self_1187968305_7343.append(element[1])
 			elif (element[0] == "A"):
 				arg_number=element[1]
 				if (len(arguments) <= arg_number):
 					raise ERR_MISSING_ARGUMENTS
 				elif True:
-					self_1187967376_8558=res
-					self_1187967376_8558.append(arguments[arg_number])
+					self_1187968305_7323=res
+					self_1187968305_7323.append(arguments[arg_number])
 			elif (element[0] == "O"):
 				arg_number=element[1]
 				if (len(arguments) <= arg_number):
 					if (element[2] == ":"):
-						self_1187967376_8575=res
-						self_1187967376_8575.append((("`" + element[3]) + "`"))
+						self_1187968305_7423=res
+						self_1187968305_7423.append((("`" + element[3]) + "`"))
 					elif True:
-						self_1187967376_8687=res
-						self_1187967376_8687.append(element[3])
+						self_1187968305_7416=res
+						self_1187968305_7416.append(element[3])
 				elif True:
-					self_1187967376_869=res
-					self_1187967376_869.append(arguments[arg_number])
+					self_1187968305_742=res
+					self_1187968305_742.append(arguments[arg_number])
 		return "".join(res)
+	
+	def describe(self):
+		return " ".join([self.name, "\t", self.expression])
 	
 
 class Parser(object):
@@ -107,8 +110,8 @@ class Parser(object):
 				command_desc=line.split(":", 1)[1]
 				command_name = command_name.replace("\t", "").strip()
 				command_desc = command_desc.strip()
-				self_1187967376_8781=commands
-				self_1187967376_8781.append(Command(command_name, command_desc))
+				self_1187968305_7512=commands
+				self_1187968305_7512.append(Command(command_name, command_desc))
 		return commands
 	
 
@@ -129,13 +132,21 @@ class Interpreter(object):
 		command_name=arguments[0]
 		command=self.commandWithName(command_name)
 		script=command.fill(arguments[1:])
+		print (("do:" + script))
 		os.system(script)
+	
+	def listCommands(self):
+		for c in self.commands:
+			print (c.describe())
 	
 
 def __main__ (args):
 	self=__module__
 	i=Interpreter()
-	i.run(args[1:])
+	if (len(args) == 1):
+		i.listCommands()
+	elif True:
+		i.run(args[1:])
 
 
 if __name__ == "__main__":
--- Makefile
+++ Makefile
@@ -2,8 +2,9 @@ SUGAR=sugar
 SOURCES=Sources/docommand.spy
 PY_MODULE=Distribution/docommand.py
 
+dist:
+	$(SUGAR) -clpy $(SOURCES) > $(PY_MODULE)
 clean:
 	rm _*.py *.pyc
 
-build:
-	$(SUGAR) -clpy $(SOURCES) > $(PY_MODULE)
+

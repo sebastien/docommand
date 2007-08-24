ee438dec3f0024336a23a93feac83d68325a0ddd
Sebastien Pierre <sebastien@type-z.org>
2007-08-24 11:40:21 -0400

    Fixed variable expression (when they need to be popen'ed) tags:        fix

--- Distribution/docommand.py
+++ Distribution/docommand.py
@@ -7,7 +7,7 @@ __module_name__ = 'docommand'
 __version__ = '0.1.0'
 ERR_MISSING_ARGUMENTS = "Missing arguments"
 class Command(object):
-	RE_ARGUMENT = re.compile("\${(\d+)([\:=]([^}]+))?}")
+	RE_ARGUMENT = re.compile("\${(\d+)(([\:=])([^}]+))?}")
 	def __init__ (self, name, expression):
 		self.name = ""
 		self.expression = ""
@@ -24,26 +24,26 @@ class Command(object):
 			match=self.__class__.RE_ARGUMENT.search(expression, offset)
 			if match:
 				argument_number = int(match.group(1))
-				default_argument = match.group(3)
-				default_argument_type = match.group(2)
-				self_1187969458_7189=res
-				self_1187969458_7189.append(["T", expression[offset:match.start()]])
+				default_argument = match.group(4)
+				default_argument_type = match.group(3)
+				self_1187969965_5487=res
+				self_1187969965_5487.append(["T", expression[offset:match.start()]])
 				if default_argument:
-					self_1187969458_713=res
-					self_1187969458_713.append(["O", argument_number, default_argument_type, default_argument])
+					self_1187969965_5432=res
+					self_1187969965_5432.append(["O", argument_number, default_argument_type, default_argument])
 				elif True:
-					self_1187969458_7126=res
-					self_1187969458_7126.append(["A", argument_number, None, None])
+					self_1187969965_5435=res
+					self_1187969965_5435.append(["A", argument_number, None, None])
 				offset = match.end()
 			elif True:
-				self_1187969458_7231=res
-				self_1187969458_7231.append(["T", expression[offset:]])
+				self_1187969965_5570=res
+				self_1187969965_5570.append(["T", expression[offset:]])
 				offset = len(expression)
 		return res
 	
 	def addArgument(self, defaultValue):
-		self_1187969458_7212=self.arguments
-		self_1187969458_7212.append((defaultValue or ""))
+		self_1187969965_5557=self.arguments
+		self_1187969965_5557.append((defaultValue or ""))
 	
 	"""Returns a list representing the initialized arguments from the given
 	(optional list of argument). When no argument is given, the the default
@@ -52,11 +52,11 @@ class Command(object):
 		if arguments is None: arguments = []
 		res=[]
 		for a in arguments:
-			self_1187969458_7253=res
-			self_1187969458_7253.append(a)
+			self_1187969965_551=res
+			self_1187969965_551.append(a)
 		while (len(res) < len(self.arguments)):
-			self_1187969458_7325=res
-			self_1187969458_7325.append(self.arguments[(len(res) - 1)])
+			self_1187969965_5662=res
+			self_1187969965_5662.append(self.arguments[(len(res) - 1)])
 		return res
 	
 	def getArity(self):
@@ -70,27 +70,31 @@ class Command(object):
 		arg_count=0
 		for element in self.parsedExpression:
 			if (element[0] == "T"):
-				self_1187969458_7396=res
-				self_1187969458_7396.append(element[1])
+				self_1187969965_5615=res
+				self_1187969965_5615.append(element[1])
 			elif (element[0] == "A"):
 				arg_number=element[1]
 				if (len(arguments) <= arg_number):
 					raise ERR_MISSING_ARGUMENTS
 				elif True:
-					self_1187969458_7319=res
-					self_1187969458_7319.append(arguments[arg_number])
+					self_1187969965_5697=res
+					self_1187969965_5697.append(arguments[arg_number])
 			elif (element[0] == "O"):
 				arg_number=element[1]
 				if (len(arguments) <= arg_number):
+					print (element)
 					if (element[2] == ":"):
-						self_1187969458_74100=res
-						self_1187969458_74100.append((("`" + element[3]) + "`"))
+						result=os.popen(element[3]).read()
+						if (result and (result[-1] == "\n")):
+							result = result[0:-1]
+						self_1187969965_5769=res
+						self_1187969965_5769.append(result)
 					elif True:
-						self_1187969458_7425=res
-						self_1187969458_7425.append(element[3])
+						self_1187969965_5735=res
+						self_1187969965_5735.append(element[3])
 				elif True:
-					self_1187969458_7433=res
-					self_1187969458_7433.append(arguments[arg_number])
+					self_1187969965_5741=res
+					self_1187969965_5741.append(arguments[arg_number])
 		return "".join(res)
 	
 	def describe(self):
@@ -110,8 +114,8 @@ class Parser(object):
 				command_desc=line.split(":", 1)[1]
 				command_name = command_name.replace("\t", "").strip()
 				command_desc = command_desc.strip()
-				self_1187969458_7537=commands
-				self_1187969458_7537.append(Command(command_name, command_desc))
+				self_1187969965_5843=commands
+				self_1187969965_5843.append(Command(command_name, command_desc))
 		return commands
 	
 
